97760a3d275687bc0eabac81161a776d92c85d28
PengHuang <penghuang@zju.edu.cn>
2019-11-30 21:57:17 +0800

    google translate tools

---
+++ Google_Translate_For_python/googleTranslate.py
@@ -0,0 +1,29 @@
+"""
+Get translate from Google Translate
+author: "Peng Huang"
+USAGE:
+    python3 googleTranslate.py <text to be translated>
+eg:
+    python3 googleTranslate.py "hello word"
+"""
+"""
+version 1.0
+2019.11.30
+The default output is English
+"""
+
+from googletrans import Translator
+import sys
+
+class GoogleTranslate(object):
+    def __init__(self, query_string=sys.argv[1]):
+        self.query_string = query_string.replace("\n", " ")
+        self.result = ""
+    def get_translation(self):
+        translator = Translator()
+        self.result += translator.translate(self.query_string, dest='zh-CN').text
+
+if __name__ == '__main__':
+    translation = GoogleTranslate()
+    translation.get_translation()
+    print(translation.result)
